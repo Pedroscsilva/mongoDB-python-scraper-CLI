12dad70d7d9c18cc6ea6f37ac864e791a9845a88
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-05-16 15:37:25 -0300

    feat: adding a scraper for the next page

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -27,7 +27,9 @@ def scrape_updates(html_content):
 
 # Requisito 3
 def scrape_next_page_link(html_content):
-    """Seu código deve vir aqui"""
+    selector = Selector(text=html_content)
+    next_link = selector.css("a.next::attr(href)").get()
+    return next_link
 
 
 # Requisito 4
