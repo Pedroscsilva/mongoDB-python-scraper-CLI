cc109cd73ef15314c28ad6a44ba2b613c5e2ec42
Pedroscsilva <pedroscsilva2001@gmail.com>
2023-05-16 21:09:19 -0300

    feat: adding a method to check for category ratings

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,3 +1,11 @@
-# Requisito 10
+from tech_news.database import find_news
+from collections import Counter
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
+    news = find_news()
+    categories = [item["category"] for item in news]
+    category_count = Counter(categories)
+    sorted_categories = sorted(category_count.keys(),
+                               key=lambda x: (-category_count[x], x))
+    return sorted_categories[:5]
